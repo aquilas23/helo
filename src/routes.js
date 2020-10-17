ef5614691ab7a2b28329a8d88f09d41244775765
aquilas91 <agaq2020@gmail.com>
2020-10-17 10:11:55 -0500

    completed

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import Auth from './Components/Auth/Auth';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Form from './Components/Form/Form';
-import Post from './Components/Post/Post';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Auth from "./Components/Auth/Auth";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Form from "./Components/Form/Form";
+import Post from "./Components/Post/Post";
 
 export default (
-    <Switch>
-        <Route exact path='/' component={Form } />
-        <Route path='/dashboard' component={Dashboard} />
-        <Route path='/newpost' component={Post } />
-        <Route path='/newpost/:id' component={Post } />
-        <Route path='/logout' component={Form } />
-    </Switch>
-)
+  <Switch>
+    <Route exact path="/" component={Auth} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/newpost" component={Post} />
+    <Route path="/newpost/:id" component={Post} />
+    <Route path="/logout" component={Auth} />
+  </Switch>
+);
